82ac96dbdf286bf928b93549b6e64d237d060309
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-09 07:53:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,3 +5,7 @@ class Application
 def call(env)
   resp = Rack::Response.new
   req = Rack::Response.new(env)
+  
+  if req.path.match(/item/)
+      item = req.params["item"]
+      if @@items.include? item
