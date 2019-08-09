52d237ca4f53174c06325a5dcc69ba1bd383b27a
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-09 08:08:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ def call(env)
   
   if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find{|s| s.name == item_name}
+      IF item = @@items.find{|s| s.name == item_name}
       
       resp.write item.name
   elsif req.path.match(/price/)
